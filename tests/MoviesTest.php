6a36fc3abab4bbcc9c068049524a9dba849c1ff7
ngonguyen <tanngontn@gmailc.com>
2022-03-09 04:58:13 +0700

    updated

--- tests/MoviesTest.php
+++ tests/MoviesTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests;
 require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../app/config/const.php';
 use App\Model\Movie as Movie;
 use App\Services\MovieServices as MovieServices; 
 use PHPUnit\Framework\TestCase;
@@ -53,16 +54,6 @@ class MoviesTest extends TestCase
         );
         $result = $this->imovieServices->filterMovies($movies,"Animation","20:00:00+11:00");
         $this->assertTrue(intval($result[0]->rating) < intval($result[1]->rating) );
-    }
-    public function testprintMovies_Movies_ReturnNoRecomendedMovies()
-    {
-        $this->imovieServices = new MovieServices();
-        $movies = array(
-            new Movie("test movies",92,["Animation","Comedy"],["19:00:00+11:00"]),
-            new Movie("test movies2",92,["Animation","Comedy"],["21:00:00+11:00"])
-        );
-        $result = $this->imovieServices->filterMovies($movies,"Animation","18:00:00+11:00");
-        $this->assertTrue($result[0]->name == "test movies"  );
     }
 
 }
